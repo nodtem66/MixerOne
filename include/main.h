05ecbcc06f7f45e20b6b58689e4ae4b13f8096ae
Jirawat I <nodtem66@gmail.com>
2019-03-05 13:58:01 +0700

    finish freeRTOS but still problem

--- include/main.h
+++ include/main.h
@@ -1,11 +1,27 @@
 #ifndef __MAIN__H_
 #define __MAIN__H_
 
+// FreeRTOS Config
+#define configUSE_TASK_NOTIFICATIONS    1
+#define configASSERT(x) if ((x) == 0) on_assert(__FILE__, __LINE__, #x)
+#define INCLUDE_vTaskDelete 1
+#define INCLUDE_xTaskResumeAll 1
+
 #include "util.h"
 #include <Arduino.h>
 #include <WString.h>
 #include <LiquidCrystal_I2C.h>
 #include <MapleFreeRTOS900.h>
+
+#define NEXT_BUTTON_PIN PA1
+#define PREV_BUTTON_PIN PA0
+#define REV_MOTOR_PIN PB0
+#define ROT_MOTOR_PIN PB1
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+#define BUTTON_PRESSED_EVENT_DELAY_MILLIS 300
+#define PWM_PERCENT_TO_SPEED(x) ((x) * 65535 / 100.0)
 
 #ifdef PRODUCTION
 #define UART Serial1
@@ -13,28 +29,27 @@
 #define UART Serial
 #endif
 
-
 #define NOT_FOUND -1
 #define PARSE_CMD_MOTOR1    0
 #define PARSE_CMD_MOTOR2    1
 #define PARSE_CMD_DURATION  2
-#define MY_ASSERT(x) if ((x) == 0) on_assert(__FILE__, __LINE__, #x)
-
-#define NEXT_BUTTON_PIN PA1
-#define PREV_BUTTON_PIN PA0
-#define REV_MOTOR_PIN PB0
-#define ROT_MOTOR_PIN PB1
-
-#define BUTTON_PRESSED_EVENT_DELAY_MILLIS 300
-#define PWM_PERCENT_TO_SPEED(x) ((x) * 65535 / 100.0)
+#define MY_ASSERT(x) configASSERT(x)
 
 #define IS_DIGIT(x) ((x) >= '0' && (x) <= '9')
 #define IS_LAST_INDEX(x,y) ((x) == ((y) - 1))
 
-#define STATE_HOME 0
-#define STATE_VIEW 1
-#define STATE_RUNNING 2
-#define STATE_TEST 3
+#define STATE_HOME      0
+#define STATE_VIEW      1
+#define STATE_RUNNING   2
+#define STATE_TEST      3
+#define BIT_NEXT_BUTTON     (1 << 0)
+#define BIT_PREV_BUTTON     (1 << 1)
+#define BIT_STATE_HOME      (1 << 2)
+#define BIT_STATE_VIEW      (1 << 3)
+#define BIT_STATE_RUNNING   (1 << 4)
+#define BIT_STATE_TEST      (1 << 5)
+#define IS_BIT_SET(x,b) ((x & b) != 0)
+
 uint8_t state = STATE_HOME;
 
 String serial_rx = "";
@@ -42,35 +57,50 @@ bool session_running = false;
 uint8_t session_page = 0;
 uint32_t pressed_event_millis = 0;
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+static TaskHandle_t displayNotify = NULL;
+uint32_t notifiedValue = 0;
 
 #include "EEPROM_helper.h"
 
 void on_assert(const char* filename, uint16_t line, const char* expr) {
 
     while(1) {
+        taskENTER_CRITICAL();
         UART.print("Assert: ");
         UART.print(expr);
         UART.print(" ");
         UART.print(filename);
         UART.print(" Line ");
         UART.println(line);
-
-        delay(1000);
+        taskEXIT_CRITICAL();
+        digitalWrite(LED_BUILTIN, LOW); delay(100);
+        digitalWrite(LED_BUILTIN, HIGH); delay(100);
+        digitalWrite(LED_BUILTIN, LOW); delay(100);
+        digitalWrite(LED_BUILTIN, HIGH); delay(100);
+        digitalWrite(LED_BUILTIN, LOW); delay(100);
+        digitalWrite(LED_BUILTIN, HIGH); delay(100);
+        digitalWrite(LED_BUILTIN, LOW); delay(100);
+        digitalWrite(LED_BUILTIN, HIGH); delay(100);
+        digitalWrite(LED_BUILTIN, LOW); delay(100);
+        digitalWrite(LED_BUILTIN, HIGH); delay(100);
     }
 
 }
 
 void on_push_button(void) {
+    UBaseType_t state = taskENTER_CRITICAL_FROM_ISR();
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
     if (millis() - pressed_event_millis > BUTTON_PRESSED_EVENT_DELAY_MILLIS) {
         if (digitalRead(NEXT_BUTTON_PIN) == LOW) {
-            //event_ISR_signal(next_button_pressed_event);
+            xTaskNotifyFromISR(displayNotify, BIT_NEXT_BUTTON, eSetBits, &xHigherPriorityTaskWoken);
         }
         if (digitalRead(PREV_BUTTON_PIN) == LOW) {
-            //event_ISR_signal(prev_button_pressed_event);
+            xTaskNotifyFromISR(displayNotify, BIT_PREV_BUTTON, eSetBits, &xHigherPriorityTaskWoken);
         }
     }
     pressed_event_millis = millis();
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    taskEXIT_CRITICAL_FROM_ISR(state);
 }
 
 void turn_on_motor(uint16_t speed1, uint16_t speed2) {
@@ -84,35 +114,48 @@ void turn_off_motor() {
 }
 
 void show_session(session_t *list, int length) {
+    taskENTER_CRITICAL();
     UART.print("Total Session: "); UART.println(length);
+    taskEXIT_CRITICAL();
     for (int i=0; i<length; i++) {
+        taskENTER_CRITICAL();
         UART.print("Session "); UART.print(i+1);
         UART.print("   Revolution Speed: "); UART.print(list[i].motor1_pwm);
         UART.print("%  Rotation Speed:   "); UART.print(list[i].motor2_pwm);
         UART.print("%  Duration (min):   "); UART.println(list[i].duration_minute);
+        taskEXIT_CRITICAL();
     }
 }
 
 void show_display_menu(int page=0) {
     if (page > 0) {
+        taskENTER_CRITICAL();
         lcd.setCursor(0, 3);
         lcd.print("< Prev");
+        taskEXIT_CRITICAL();
     }
     if (IS_LAST_INDEX(page, session_length)) {
+        taskENTER_CRITICAL();
         lcd.setCursor(15, 3);
         lcd.print("Run >");
+        taskEXIT_CRITICAL();
     } else {
+        taskENTER_CRITICAL();
         lcd.setCursor(14, 3);
         lcd.print("Next >");
+        taskEXIT_CRITICAL();
     }
 }
 
 void show_running_menu() {
+    taskENTER_CRITICAL();
     lcd.setCursor(0, 3);
     lcd.print("< Resume     Pause >");
+    taskEXIT_CRITICAL();
 }
 
 void show_timer(uint16_t minute, uint8_t second) {
+    taskENTER_CRITICAL();
     lcd.setCursor(7, 2);
     lcd.print("             ");
     lcd.setCursor(7, 2);
@@ -120,28 +163,43 @@ void show_timer(uint16_t minute, uint8_t second) {
     lcd.print("m");
     lcd.print(second);
     lcd.print("s");
+    taskEXIT_CRITICAL();
 }
 
 void lcd_display_home() {
+    vTaskSuspendAll();
     lcd.clear();
     lcd.setCursor(3,1);
     lcd.print("MixerOne v1.0");
     lcd.setCursor(0,3);
     lcd.print("< Clear       Load >");
+    xTaskResumeAll();
 }
 
 void lcd_display_test(uint16_t a, uint16_t b) {
-    lcd.clear();
-    lcd.home();
+    vTaskSuspendAll();
+    //lcd.command(LCD_CLEARDISPLAY);
+    //delayMicroseconds(2000);
+    //lcd.command(LCD_RETURNHOME);
+    //delayMicroseconds(2000);
+    UART.print(systick_uptime());
+    delayMicroseconds(100);
+    UART.print('.');
+    lcd.setCursor(0, 0);
+    UART.print(';');
+    /*
     lcd.print("TEST PWM");
     lcd.setCursor(0, 1);
     lcd.print("Revolution: "); lcd.print(a);
     lcd.setCursor(0, 2);
     lcd.print("Rotation:   "); lcd.print(b);
+    */
+    xTaskResumeAll();
 }
 
 void lcd_display_session(int page=0) {
     if (page < session_length) {
+        taskENTER_CRITICAL();
         lcd.clear();
         lcd.home();
         lcd.print("Session "); lcd.print(page+1); lcd.print(" "); lcd.print(session_list[page].duration_minute); lcd.print("min");
@@ -149,6 +207,7 @@ void lcd_display_session(int page=0) {
         lcd.print("Revolution: "); lcd.print(session_list[page].motor1_pwm); lcd.print("%");
         lcd.setCursor(0, 2);
         lcd.print("Rotation:   "); lcd.print(session_list[page].motor2_pwm); lcd.print("%");
+        taskEXIT_CRITICAL();
         show_display_menu(page);
         session_page = page;
     }
@@ -156,6 +215,7 @@ void lcd_display_session(int page=0) {
 
 void lcd_display_running(int page=0) {
     if (page < session_length) {
+        taskENTER_CRITICAL();
         lcd.clear();
         lcd.home();
         lcd.print("Session "); lcd.print(page+1); lcd.print(" "); lcd.print(session_list[page].duration_minute); lcd.print("min");
@@ -165,6 +225,7 @@ void lcd_display_running(int page=0) {
         lcd.print("Rot:   "); lcd.print(session_list[page].motor2_pwm); lcd.print("%");
         lcd.setCursor(0, 2);
         lcd.print("Left: "); 
+        taskEXIT_CRITICAL();
         show_timer(session_list[page].duration_minute, 0);
         show_running_menu();
     }
@@ -241,8 +302,9 @@ void test_pwm(String *cmd) {
             if (!IS_DIGIT(b)) break;
             cycle2 = cycle2*10 + (b - '0');
         }
-        turn_on_motor(cycle1, cycle2);
-
+        taskENTER_CRITICAL();
+        //turn_on_motor(cycle1, cycle2);
+        taskEXIT_CRITICAL();
         lcd_display_test(cycle1, cycle2);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include "main.h"
-
 void gpio_init(void) {
     turn_off_motor();
-    
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(REV_MOTOR_PIN, PWM);
     pinMode(ROT_MOTOR_PIN, PWM);
-    Timer3.setPrescaleFactor(22);
+    Timer3.setPrescaleFactor(36);
     pinMode(NEXT_BUTTON_PIN, INPUT_PULLUP);
     pinMode(PREV_BUTTON_PIN, INPUT_PULLUP);
     attachInterrupt(NEXT_BUTTON_PIN, on_push_button, FALLING);
@@ -14,62 +12,37 @@ void gpio_init(void) {
     
     UART.begin(9600);
     
-    //lcd.init();
-    //lcd.backlight();
+    lcd.init();
+    lcd.backlight();
 
     ASSERT( EEPROM.init() == EEPROM_OK );
     load_EEPROM();
 }
 
-static void status_led_task(void* args) {
+void status_led_task(void* args) {
     for (;;) {
         digitalWrite(LED_BUILTIN, LOW);
-        vTaskDelay(configTICK_RATE_HZ);
+        vTaskDelay(pdMS_TO_TICKS(500));
         digitalWrite(LED_BUILTIN, HIGH);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+    vTaskDelete( NULL );
+}
+
+void test_task(void* args) {
+    for (;;) {
+        turn_off_motor();
+        vTaskDelay(configTICK_RATE_HZ);
+        turn_on_motor(65535, 65535);
         vTaskDelay(configTICK_RATE_HZ);
     }
+    vTaskDelete( NULL );
 }
 
-void display_home_task(void) {
-    
-    for (;;) {
-        state = STATE_HOME;
-        lcd_display_home();
-        /*
-        do {
-            //event_wait_multiple(0, home_event, next_button_pressed_event, prev_button_pressed_event);
-            //evt = event_last_signaled_get();
-        } while (evt != home_event && state != STATE_HOME);
-        
-        if (evt == next_button_pressed_event) {
-            load_EEPROM();
-            if (session_length > 0) {
-                event_signal(session_display_event);
-                event_wait(home_event);
-            } else {
-                lcd.setCursor(0,2);
-                lcd.print("     Not found");
-                task_wait(3000);
-            }
-        } else if (evt == prev_button_pressed_event) {
-            if (clear_EEPROM() == EEPROM_OK) {
-                lcd.setCursor(0,2);
-                lcd.print("      Success");
-            } else {
-                lcd.setCursor(0,2);
-                lcd.print("       Error");
-                
-            }
-            task_wait(3000);
-        }*/
-    }
-}
-
-/*
-void serial_command_task(void) {
+void serial_command_task(void* args) {
     static byte b;
     static uint8_t success = 0;
-    task_open();
+    
     for (;;) {
         while (UART.available() > 0) {
             b = UART.read();
@@ -82,17 +55,23 @@ void serial_command_task(void) {
             } else if (serial_rx.startsWith(F("HI"))) {
                 UART.println(F("HI"));
             } else if (serial_rx.startsWith(F("T"))) {
+                UART.println(F("OK"));
                 if (!session_running) {
+                    vTaskDelay(pdMS_TO_TICKS(100));
                     test_pwm(&serial_rx);
-                    event_signal(test_pwm_event);
+                    vTaskDelay(pdMS_TO_TICKS(100));
+                    //xTaskNotify(displayNotify, BIT_STATE_TEST, eSetBits);
+                    UART.println("!!");
                 }
-            } else {
+            } else if (serial_rx.length() > 0) {
                 success = parse_command(&serial_rx, session_list, &session_length);
                 if (success == 0)  {
                     UART.println(F("OK"));
                     if (!session_running) {
-                        event_signal(session_display_event);
+                        taskENTER_CRITICAL();
                         save_EEPROM();
+                        taskEXIT_CRITICAL();
+                        //xTaskNotify(displayNotify, BIT_STATE_VIEW, eSetBits);
                     }
                 }
                 else { UART.print(F("ERROR ")); UART.println(success); }
@@ -100,115 +79,161 @@ void serial_command_task(void) {
             serial_rx = "";
             b = '\0';
         }
-        task_wait(100);
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
-    task_close();
+    vTaskDelete( NULL );
 }
 
-void test_pwm_task(void) {
-    static Evt_t evt;
-    task_open();
-    event_wait(test_pwm_event);
-    for (;;) {
-        state = STATE_TEST;
-        do {
-            event_wait_multiple(0, test_pwm_event, next_button_pressed_event, prev_button_pressed_event);
-            evt = event_last_signaled_get();
-        } while (evt != test_pwm_event && state != STATE_TEST);
-
-        if ( evt != test_pwm_event ) {
-            turn_off_motor();
-            event_signal(home_event);
-            event_wait(test_pwm_event);
+static inline void display_home() {
+    lcd_display_home();
+    BaseType_t status = xTaskNotifyWait(0x00, UINT32_MAX, &notifiedValue, portMAX_DELAY);
+    if (status == pdPASS) {
+        if ( IS_BIT_SET(notifiedValue, BIT_NEXT_BUTTON) ) {
+            taskENTER_CRITICAL();
+            load_EEPROM();
+            taskEXIT_CRITICAL();
+            if (session_length > 0) {
+                state = STATE_VIEW;
+            } else {
+                lcd.setCursor(0,2);
+                lcd.print("     Not found");
+                vTaskDelay(pdMS_TO_TICKS(1000));
+            }
+        }
+        else if ( IS_BIT_SET(notifiedValue, BIT_PREV_BUTTON) ) {
+            taskENTER_CRITICAL();
+            status = clear_EEPROM();
+            taskEXIT_CRITICAL();
+            if (status == EEPROM_OK) {
+                lcd.setCursor(0,2);
+                lcd.print("      Success");
+                vTaskDelay(pdMS_TO_TICKS(1000));
+            } else {
+                lcd.setCursor(0,2);
+                lcd.print("       Error");
+                vTaskDelay(pdMS_TO_TICKS(1000));
+            }
+        }
+        if ( IS_BIT_SET(notifiedValue, BIT_STATE_TEST) ) {
+            state = STATE_TEST;
+        }
+        else if ( IS_BIT_SET(notifiedValue, BIT_STATE_VIEW) ) {
+            state = STATE_VIEW;
         }
     }
-    task_close();
 }
 
-void session_display_task(void) {
-    static uint8_t i = 0;
-    static Evt_t evt = 0;
-    task_open();
-    event_wait(session_display_event);
-    for(;;) {
-        state = STATE_VIEW;
-        for(i=0; i<session_length;) {
-            lcd_display_session(i);
-            do {
-                event_wait_multiple(0, session_display_event, next_button_pressed_event, prev_button_pressed_event);
-                evt = event_last_signaled_get();
-            } while (evt != session_display_event && state != STATE_VIEW);
+static inline void display_testpwm(void) {
+    if (xTaskNotifyWait(0x00, UINT32_MAX, &notifiedValue, portMAX_DELAY) == pdPASS) {
+        if ( IS_BIT_SET(notifiedValue, BIT_NEXT_BUTTON) || IS_BIT_SET(notifiedValue, BIT_PREV_BUTTON)) {
+            state = STATE_HOME;
+        } else if ( IS_BIT_SET(notifiedValue, BIT_STATE_VIEW) ) {
+            state = STATE_VIEW;
+        }
+    }
+}
 
-            if (evt == session_display_event) break;
-            else if (evt == next_button_pressed_event) {
+static inline void display_session(void) {
+    for(uint8_t i=0; i<session_length;) {
+        lcd_display_session(i);
+        if (xTaskNotifyWait(0x00, UINT32_MAX, &notifiedValue, portMAX_DELAY) == pdPASS) {
+            if ( IS_BIT_SET(notifiedValue, BIT_STATE_VIEW) ) {
+                state = STATE_VIEW;
+                break;
+            } else if ( IS_BIT_SET(notifiedValue, BIT_STATE_TEST) ) {
+                state = STATE_TEST;
+                break;
+            }
+            if ( IS_BIT_SET(notifiedValue, BIT_NEXT_BUTTON) ) {
                 i++;
                 if (i >= session_length) {
-                    event_signal(session_running_event);
-                    event_wait(session_display_event);
+                    state = STATE_RUNNING;
                     break;
                 }
-            } else if (evt == prev_button_pressed_event) {
+            } else if ( IS_BIT_SET(notifiedValue, BIT_PREV_BUTTON) ) {
                 if (i-1 >= 0) i--;
             }
         }
     }
-    task_close();
 }
 
-void session_running_task(void) {
+static inline void display_running(void) {
     static uint16_t minute = 0;
     static int8_t second = 0;
-    for (;;) {
-        session_running = true;
-        state = STATE_RUNNING;
-        for (session_page = 0;session_page < session_length; session_page++) {
-            lcd_display_running(session_page);
-            turn_on_motor(
-                PWM_PERCENT_TO_SPEED(session_list[session_page].motor1_pwm),
-                PWM_PERCENT_TO_SPEED(session_list[session_page].motor2_pwm)
-            );
-            minute = session_list[session_page].duration_minute - 1;
-            for (; minute >=0; minute--) {
-                for (second = 60; second >= 0;) {
-                    show_timer(minute, second);
+    session_running = true;
+    for (session_page = 0;session_page < session_length; session_page++) {
+        lcd_display_running(session_page);
+        turn_on_motor(
+            PWM_PERCENT_TO_SPEED(session_list[session_page].motor1_pwm),
+            PWM_PERCENT_TO_SPEED(session_list[session_page].motor2_pwm)
+        );
+        minute = session_list[session_page].duration_minute - 1;
+        for (; minute >=0; minute--) {
+            for (second = 60; second >= 0;) {
+                if (session_running) show_timer(minute, second);
+
+                // Wait for PAUSE or RESUME
+                if (xTaskNotifyWait(0x00, UINT32_MAX, &notifiedValue, pdMS_TO_TICKS(1000)) == pdPASS) {
                     // WHEN PAUSE
-                    if (event_last_signaled_get() == next_button_pressed_event) {
-                        session_running = false;
-                        lcd.setCursor(13, 3);
-                        lcd.print("  End >");
-                        turn_off_motor();
-                    }
-                    if (session_running) second--;
-                    else {
-                        event_wait_multiple(0, prev_button_pressed_event, next_button_pressed_event);
-                        if (event_last_signaled_get() == next_button_pressed_event) {
-                            goto finish;
-                        } else {
-                            // WHEN RESUME
-                            session_running = true;
+                    if ( IS_BIT_SET(notifiedValue, BIT_NEXT_BUTTON) ) {
+                        if (session_running) {
+                            session_running = false;
                             lcd.setCursor(13, 3);
-                            lcd.print("Pause >");
-                            turn_on_motor(
-                                PWM_PERCENT_TO_SPEED(session_list[session_page].motor1_pwm),
-                                PWM_PERCENT_TO_SPEED(session_list[session_page].motor2_pwm)
-                            );
+                            lcd.print("  End >");
+                            turn_off_motor();
+                        } else {
+                            state = STATE_VIEW;
+                            goto finish;
                         }
                     }
+                    // WHEN RESUME
+                    else if ( IS_BIT_SET(notifiedValue, BIT_PREV_BUTTON) ) {
+                        session_running = true;
+                        lcd.setCursor(13, 3);
+                        lcd.print("Pause >");
+                        turn_on_motor(
+                            PWM_PERCENT_TO_SPEED(session_list[session_page].motor1_pwm),
+                            PWM_PERCENT_TO_SPEED(session_list[session_page].motor2_pwm)
+                        );
+                    }
                 }
-                if (minute == 0) break;
+                if (session_running) second--;
             }
+            if (minute == 0) break;
         }
-finish:
-        session_running = false;
     }
-}*/
+finish:
+    session_running = false;
+}
+
+void display_task(void* args) {
+    state = STATE_HOME;
+    for(;;) {
+        switch (state) {
+            case STATE_HOME:
+                display_home();
+                break;
+            case STATE_TEST:
+                display_testpwm();
+                break;
+            case STATE_VIEW:
+                display_session();
+                break;
+            case STATE_RUNNING:
+                display_running();
+                break;
+        }
+    }
+    vTaskDelete( NULL );
+}
 
 /*=== START Main CODE =================================*/
 int main(void) {
     gpio_init();
-    portBASE_TYPE s1;
-    s1 = xTaskCreate(status_led_task, "LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    MY_ASSERT(s1 == pdPASS);
+    MY_ASSERT(xTaskCreate(status_led_task, "LED", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL) == pdPASS);
+    MY_ASSERT(xTaskCreate(test_task, "LED2", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL) == pdPASS);
+    MY_ASSERT(xTaskCreate(serial_command_task, "serial", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+3, NULL) == pdPASS);
+    MY_ASSERT(xTaskCreate(display_task, "display", configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+2, &displayNotify) == pdPASS);
     vTaskStartScheduler();
     return 0;
 }
